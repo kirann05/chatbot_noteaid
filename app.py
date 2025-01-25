cc362a09e6f84e7c6380df92a77d633494ee444f
Kiran G <stoic@Kirans-MacBook-Air.local>
2025-01-25 16:16:51 -0500

    Fix: Properly define and use PORT environment variable

--- app.py
+++ app.py
@@ -102,5 +102,5 @@ def static_files(filename):
 
 
 if __name__ == "__main__":
-   port = int(os.environ.get("PORT", 8000))  # Default to 8000 if PORT is not set
-app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get("PORT", 8000))  # Default to 8000 if PORT is not set
+    app.run(host="0.0.0.0", port=port, debug=True)
